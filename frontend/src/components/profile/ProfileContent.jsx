367d25d013f9f5aadab239fed750c63cec740fce
Vivek Mishra <vivekmishra9343@gmail.com>
2024-10-20 04:43:03 +0530

    enhancing profile

---
+++ frontend/src/components/profile/ProfileContent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatar from "./Avatar";
+import ProfileForm from "./ProfileForm";
+
+const ProfileContent = () => {
+  return (
+    <div className='Rectangle147 w-[1144px] h-[918px] left-[273px] top-[53px] absolute bg-[#fefdff] rounded-[20px]'>
+      <Avatar />
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default ProfileContent;
